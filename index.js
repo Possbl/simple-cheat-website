e864e6bed764777263b8ff786ff9bafe2eb6771d
Possible <78051846+Possbl@users.noreply.github.com>
2023-06-28 00:43:12 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const cardElements = document.querySelectorAll('.glowing-div');
+
+    cardElements.forEach(function(cardElement) {
+        cardElement.addEventListener('mouseenter', function() {
+            cardElement.classList.add('fa-beat');
+            console.log('working');
+        });
+
+        cardElement.addEventListener('mouseleave', function() {
+            cardElement.classList.remove('fa-beat');
+        });
+    });
+});
+
